89cfd31e700900755f923c5de2d5aa5dd9ba5228
sumphp <drass1022@naver.com>
2025-10-20 23:56:41 +0900

    e

--- Android Project/sumphp/src/main/java/com/example/sumphp/MainActivity.kt
+++ Android Project/sumphp/src/main/java/com/example/sumphp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sumphp.ui.theme.SumphpTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SumphpTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+@Composable()
+fun HomeScreen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("나의 취미",
+            style = MaterialTheme.typography.headlineMedium)
+        Image(
+            painter = painterResource(id = com.example.sumphp.R.drawable.hobby),
+            contentDescription = "Jetpack Compose 로고",
+            modifier = Modifier
+                .size(300.dp)
+                .padding(16.dp)
+        )
+        Text("음악 감상!",
+            style = MaterialTheme.typography.headlineMedium)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SumphpTheme {
-        Greeting("Android")
-    }
+fun HomeScreenPreview() {
+    HomeScreen()
 }
